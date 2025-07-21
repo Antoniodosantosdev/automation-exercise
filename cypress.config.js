9ac7272471c41ce91807b9a96b2fd873aaf40122
Antonio <ti.antoniocarllos@gmail.com>
2025-07-21 03:36:23 -0300

    Primeiro committ

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    baseUrl: "https://automationexercise.com",
+    viewportWidth: 1440,
+    viewportHeight: 900,
+
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
