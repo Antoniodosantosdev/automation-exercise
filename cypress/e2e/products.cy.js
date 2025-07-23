545827f786ceb81b37be8a0ab7186ab7bf0e979d
Antonio <ti.antoniocarllos@gmail.com>
2025-07-23 02:28:59 -0300

    finalizado testes E2E

--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -3,16 +3,12 @@ describe("Products", () => {
   beforeEach(() => {
     cy.visit("/products");
   });
-  const randomQuantity = Cypress.gerador.number.int({ min: 1, max: 10 });
+  
 
   context("Pesquisar um produto especifico", () => {
     it("Deve pesquisar por um produto especifico", () => {
       cy.fixture("productData").then((data) => {
-        
-        cy.get("#search_product").type(data.blueTop.name); 
-        cy.get("#submit_search").click();
-
-        cy.get(".productinfo").should("contain", data.blueTop.name); 
+        cy.pesquisarProduto(data.blueTop);
       });
     });
   });
@@ -20,35 +16,17 @@ describe("Products", () => {
   context("Adicionar um produto ao carrinho", () => {
     it("Deve Adiciona um produto ao carrinho", () => {
       cy.fixture("productData").then((data) => {
-        
-        cy.contains(".productinfo", data.blueTop.name) 
-          .parents(".product-image-wrapper")
-          .trigger("mouseover");
-
-        cy.contains(".productinfo", data.blueTop.name)
-          .parents(".product-image-wrapper")
-          .contains("Add to cart")
-          .click();
-
-        cy.contains("Continue Shopping").should("be.visible");
-        cy.contains("View Cart").click();
-
-        cy.get(".cart_description").should("contain", data.blueTop.name); 
+        cy.adicionarProdutoAoCarrinho(data.blueTop);
       });
     });
   });
 
-  
-
-  
   context("Pesquisa de produto usando o botão de pesquisa", () => {
     it("Deve pesquisar um produto usando o botão de pesquisa", () => {
-      cy.fixture("productData").then((data) => {
-        
-        cy.get("#search_product").type(data.blueTop.name); 
-        cy.get("#submit_search").click();
 
-        cy.get(".productinfo").should("contain", data.blueTop.name); 
+      cy.fixture("productData").then((data) => {
+        cy.pesquisarProdutoComBotao(data.winterTop );
+        
       });
     });
   });
